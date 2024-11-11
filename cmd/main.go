0edbbdde8f4bd387c53d8a9ded40f3638909b621
Abdulkadir DILSIZ <akdilsiz@tecpor.com>
2024-11-11 19:43:42 +0300

    Complete: refactor application logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/barisaydogdu/FileBackupToolCLI/handlers/cli"
 	"os"
 	"os/signal"
 	"syscall"
-
-	cli "github.com/barisaydogdu/FileBackupToolCLI/handlers/cli"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
 	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
 	go func() {
-		<-sigs //sinyal alındığında kanal tetiklenir
-		fmt.Println("\nBackup process stopping")
-		done <- true
+		select {
+		case <-sigs:
+			cancel()
+		}
 	}()
 
-	fmt.Println("The backup process has started. Press CTRL + C to exit.")
+	fmt.Println("The backup program has started. Press CTRL + C to exit.")
 
-	for {
-		select {
-		case <-done:
-			fmt.Println("Program stopped")
-			return
-		default:
-			cli.Execute()
-		}
+	app := cli.NewCLI(ctx, cancel)
+	app.Execute()
+
+	select {
+	case <-ctx.Done():
+		return
 	}
-
 }
--- handlers/cli/cli.go
+++ handlers/cli/cli.go
@@ -1,46 +1,75 @@
 package cli
 
 import (
-	"fmt"
-	"os"
-
+	"context"
 	fileProcess "github.com/barisaydogdu/FileBackupToolCLI/pkg/file"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 var (
 	sourceFile string
 	targetFile string
 	period     int64
-
-	rootCmd = &cobra.Command{
-		Use:   "filecopy",
-		Short: "A tool to copy files from source to target at a specified period",
-		Long: `This application copies a file from a source path to a target path at regular intervals.
-You can specify the source file, target file, and the frequency (in seconds) for the copy operation.`,
-	}
 )
 
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+type CLI struct {
+	ctx         context.Context
+	ctxCancel   context.CancelFunc
+	cmd         *cobra.Command
+	subCommands map[string]*cobra.Command
+	file        *fileProcess.File
+}
+
+func NewCLI(ctx context.Context, cancel context.CancelFunc) *CLI {
+	cli := &CLI{
+		ctx:       ctx,
+		ctxCancel: cancel,
+		cmd: &cobra.Command{
+			Use:   "backup",
+			Short: "A tool to copy files from source to target at a specified period",
+			Long: `This application copies a file from a source path to a target path at regular intervals.
+You can specify the source file, target file, and the frequency (in seconds) for the copy operation.`,
+		},
+		subCommands: make(map[string]*cobra.Command),
 	}
+	cli.cmd.SetContext(ctx)
+
+	cli.file = fileProcess.NewFile(ctx, cli.cmd)
+
+	cli.subCommands["backup"] = &cobra.Command{
+		Use:   "file",
+		Short: "Backup file with frequency",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cli.file.BackupFileWithPeriod(sourceFile, targetFile, period); err != nil {
+				cmd.PrintErrf("there si something error with backup file %v\n", err)
+			}
+		},
+	}
+	cli.subCommands["backup"].SetContext(ctx)
+
+	cli.cmd.PersistentFlags().StringVar(&sourceFile, "source", "", "")
+	cli.cmd.PersistentFlags().StringVar(&targetFile, "target", "", "")
+	cli.cmd.PersistentFlags().Int64Var(&period, "period", 0, "seconds")
+	cli.cmd.AddCommand(cli.subCommands["backup"])
+
+	return cli
 }
 
-func init() {
-	rootCmd.PersistentFlags().StringVar(&sourceFile, "sourcefile", "", "")
-	rootCmd.PersistentFlags().StringVar(&targetFile, "targetfile", "", "")
-	rootCmd.PersistentFlags().Int64Var(&period, "period", 0, "")
-	rootCmd.AddCommand(backupCommand)
-}
+func (c *CLI) Execute() {
+	if err := c.cmd.ExecuteContext(c.ctx); err != nil {
+		log.Fatal(err)
+	}
 
-var backupCommand = &cobra.Command{
-	Use:   "backupfile",
-	Short: "Backup file with frequency",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := fileProcess.BackupFilewithPeriod(sourceFile, targetFile, period); err != nil {
-			fmt.Errorf("There si something error with backup file %v", err)
-		}
-	},
+	if sourceFile == "" || targetFile == "" {
+		c.cmd.PrintErr("source and target files are required\n")
+		c.ctxCancel()
+		return
+	}
+
+	if period == 0 {
+		c.cmd.PrintErr("please specify a period\n")
+		c.ctxCancel()
+		return
+	}
 }
